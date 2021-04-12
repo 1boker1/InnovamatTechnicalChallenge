0c759975d1eb513382075e60b1142462794fda87
DESKTOP-KJCV6D1\Nil <nil.ll.lopez@gmail.com>
2021-04-12 19:38:18 +0200

    Adding Challenge Controller

---
+++ Assets/Scripts/NumbersChallenge/ChallengeController.cs
@@ -0,0 +1,30 @@
+﻿using InnovamatTechnicalChallenge.ConfigurationObjects;
+using UnityEngine;
+
+namespace InnovamatTechnicalChallenge.NumberGame
+{
+    public class ChallengeController : MonoBehaviour
+    {
+        public NumberToTextConfiguration enunciateConfiguration;
+        public AnswersConfiguration answerConfiguration;
+
+        public ChallengePart questionPanel;
+        public ChallengePart answerPanel;
+
+        public Challenge CurrentChallenge { get; set; }
+
+        public void StartGame()
+        {
+
+        }
+        public void NextChallenge()
+        {
+            CurrentChallenge=new Challenge(enunciateConfiguration, answerConfiguration);
+
+            questionPanel.SetUp(CurrentChallenge);
+            questionPanel.Show();
+
+            answerPanel.SetUp(CurrentChallenge);
+        }
+    }
+}
